aa57794f3ee0a765cb3f72ad47abce1d66074f6d
seyun <nkje0139@naver.com>
2024-07-03 13:22:43 +0900

    feat: 프로젝트 초기세팅

---
+++ src/main/java/sunjin/DeptManagement_BackEnd/DeptManagementApplication.java
@@ -0,0 +1,13 @@
+package sunjin.DeptManagement_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptManagementApplication.class, args);
+	}
+
+}
