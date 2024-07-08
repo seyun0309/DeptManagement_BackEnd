aa2b296d7ceb3b4ac246719ef25ff1d2f40e371a
seyun <nkje0139@naver.com>
2024-07-08 09:29:17 +0900

    feat : 전체 조회시 사용되는 DTO 추가

---
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/response/GetAllOrderDTO.java
@@ -0,0 +1,24 @@
+package sunjin.DeptManagement_BackEnd.domain.order.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import sunjin.DeptManagement_BackEnd.global.enums.ProductStatusType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class GetAllOrderDTO {
+    private String createTime;
+    private String productType;
+    private String productName;
+    private int price;
+    private int quantity;
+    private int totalPrice;
+    private ProductStatusType status;
+    private LocalDateTime processDate;
+    private String applicant;
+    private String applicantDeptName;
+}
