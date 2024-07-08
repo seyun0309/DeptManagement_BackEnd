909586c7a633cb675b5552a971ff45b4ed2bd10d
seyun <nkje0139@naver.com>
2024-07-09 08:21:06 +0900

    feat : 커스텀 예외 등록

---
+++ src/main/java/sunjin/DeptManagement_BackEnd/global/auth/exception/CustomException.java
@@ -0,0 +1,7 @@
+package sunjin.DeptManagement_BackEnd.global.auth.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
