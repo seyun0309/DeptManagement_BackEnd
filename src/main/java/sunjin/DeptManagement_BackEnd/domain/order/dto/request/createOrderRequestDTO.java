c857fcb92d4038a8e568310261c82ba71960cb22
seyun <nkje0139@naver.com>
2024-07-09 10:58:26 +0900

    style : @NotBlank message 추가

--- src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/request/createOrderRequestDTO.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/request/createOrderRequestDTO.java
@@ -14,13 +14,13 @@ public class createOrderRequestDTO {
     @NotBlank
     private String productType;
 
-    @NotBlank
+    @NotBlank(message = "주문 하시는 상품의 이름을 입력해주세요")
     private String productName;
 
-    @NotBlank
+    @NotBlank(message = "해당 상품의 개당 가격을 입력해주세요")
     private int price;
 
-    @NotBlank
+    @NotBlank(message = "총 몇 개의 상품을 구매하는지 적어주세요")
     private int quantity;
 
     // 문자열을 ProductType enum으로 변환하는 메서드
