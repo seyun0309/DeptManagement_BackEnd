6603d11c0434ee9ab2b9863691d4d1ce958f836a
seyun <nkje0139@naver.com>
2024-07-11 09:37:37 +0900

    feat : ImgUrl을 넣을 수 있는 칼럼 추가

--- src/main/java/sunjin/DeptManagement_BackEnd/domain/receipt/domain/Receipt.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/receipt/domain/Receipt.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.web.multipart.MultipartFile;
+import sunjin.DeptManagement_BackEnd.domain.department.domain.Department;
 import sunjin.DeptManagement_BackEnd.domain.member.domain.Member;
 import sunjin.DeptManagement_BackEnd.global.common.BaseEntity;
 
@@ -18,16 +20,26 @@ import java.time.LocalDateTime;
 @Table(name = "receipt")
 public class Receipt extends BaseEntity {
 
+    @Column(nullable = false)
+    private String receiptImgPath;
+
     @Column(length = 100, nullable = false)
     private String storeName;
 
     @Column(nullable = false)
-    private int rTotalPrice;
+    private int receiptPrice;
 
     @Column(nullable = false)
     private LocalDateTime recDate;
 
+    @Column
+    private String ImgURL;
+
     @JoinColumn(columnDefinition = "varchar(100)",nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
+
+    @JoinColumn(name = "department_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Department department;
 }
