d63b6845a48beade68805c06dc7689a18a23ee34
seyun <nkje0139@naver.com>
2024-07-15 11:33:39 +0900

    feat : 적요 추가 및 이름 변경

--- src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/request/createOrderRequestDTO.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/request/createOrderRequestDTO.java
@@ -2,7 +2,7 @@ package sunjin.DeptManagement_BackEnd.domain.order.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-import sunjin.DeptManagement_BackEnd.global.enums.ProductType;
+import sunjin.DeptManagement_BackEnd.global.enums.OrderType;
 
 @Getter
 @Setter
@@ -14,17 +14,17 @@ public class createOrderRequestDTO {
     @NotBlank
     private String productType;
 
-    @NotBlank(message = "주문 하시는 상품의 이름을 입력해주세요")
-    private String productName;
+    @NotBlank(message = "상호명을 입력해주세요")
+    private String storeName;
 
-    @NotBlank(message = "해당 상품의 개당 가격을 입력해주세요")
-    private int price;
+    @NotBlank(message = "총 가격을 입력해주세요")
+    private int totalPrice;
 
-    @NotBlank(message = "총 몇 개의 상품을 구매하는지 적어주세요")
-    private int quantity;
+    // 적요
+    private  String description;
 
     // 문자열을 ProductType enum으로 변환하는 메서드
-    public ProductType getProductTypeEnum() {
-        return ProductType.fromDescription(this.productType);
+    public OrderType getProductTypeEnum() {
+        return OrderType.fromDescription(this.productType);
     }
 }
