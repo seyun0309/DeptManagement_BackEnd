28dab7cd609d42ab901c2bb57d9362243affc386
seyun <nkje0139@naver.com>
2024-07-15 12:40:13 +0900

    feat : 클래스명 변경 및 내용 추가

--- src/main/java/sunjin/DeptManagement_BackEnd/global/enums/OrderType.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/global/enums/OrderType.java
@@ -3,21 +3,25 @@ package sunjin.DeptManagement_BackEnd.global.enums;
 import lombok.Getter;
 
 @Getter
-public enum ProductType {
+public enum OrderType {
     FIXTURES("fixtures","비품"),
     SNACK("snack","간식"),
+    FOOD_COSTS("food", "식비"),
+    GENERAL("general", "일반 경비"),
+    ENTERTAINMENT("entertainment", "접대비"),
+    TRANSPORTATION("transportation", "교통비"),
     ETC("etc", "기타");
 
     private final String code ;
     private final String description;
 
-    ProductType(String code, String description) {
+    OrderType(String code, String description) {
         this.code = code;
         this.description = description;
     }
 
-    public static ProductType fromDescription(String description) {
-        for (ProductType type : ProductType.values()) {
+    public static OrderType fromDescription(String description) {
+        for (OrderType type : OrderType.values()) {
             if (type.description.equalsIgnoreCase(description)) {
                 return type;
             }
