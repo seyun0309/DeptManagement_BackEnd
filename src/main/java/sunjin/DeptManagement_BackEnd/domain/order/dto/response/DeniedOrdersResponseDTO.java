5868e7c7c15730e77767cb5cf187d450aa7ce340
seyun <nkje0139@naver.com>
2024-07-15 12:41:24 +0900

    feat : 반려 상태 주문 조회시 사용되는 DTO 추가

---
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/response/DeniedOrdersResponseDTO.java
@@ -0,0 +1,22 @@
+package sunjin.DeptManagement_BackEnd.domain.order.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class DeniedOrdersResponseDTO {
+    private Long orderId;
+    private String applicantDeptName;
+    private String applicant;
+    private String productType;
+    private String storeName;
+    private int totalPrice;
+    private String description;
+    private String orderStatus;
+    private String deniedDescription;
+    private String createdAt;
+    private String updatedAt;
+}
