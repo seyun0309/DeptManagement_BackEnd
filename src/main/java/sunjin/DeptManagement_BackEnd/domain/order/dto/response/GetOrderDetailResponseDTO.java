c7371664de5b81faef6fa2c5709cdf013779bc44
seyun <nkje0139@naver.com>
2024-07-16 12:52:48 +0900

    feat : 주문 상세 조회시 사용되는 응답 DTO 추가

---
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/response/GetOrderDetailResponseDTO.java
@@ -0,0 +1,21 @@
+package sunjin.DeptManagement_BackEnd.domain.order.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class GetOrderDetailResponseDTO {
+    private String DeptName;
+    private String applicantName;
+    private String orderType;
+    private String storeName;
+    private int totalPrice;
+    private String description;
+    private String createdAt;
+    private String firstProcDate;
+    private String secondProcDate;
+    private String rejectionDescription;
+}
