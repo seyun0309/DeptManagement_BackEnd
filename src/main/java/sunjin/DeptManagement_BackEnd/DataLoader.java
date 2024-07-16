9e228278b1552ce3ca5abf950590340a710c63c9
seyun <nkje0139@naver.com>
2024-07-16 12:48:15 +0900

    feat : 더미테이블 삽입

--- src/main/java/sunjin/DeptManagement_BackEnd/DataLoader.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/DataLoader.java
@@ -10,6 +10,10 @@ import sunjin.DeptManagement_BackEnd.domain.department.domain.Department;
 import sunjin.DeptManagement_BackEnd.domain.department.repository.DepartmentRepository;
 import sunjin.DeptManagement_BackEnd.domain.member.domain.Member;
 import sunjin.DeptManagement_BackEnd.domain.member.repository.MemberRepository;
+import sunjin.DeptManagement_BackEnd.domain.order.domain.Order;
+import sunjin.DeptManagement_BackEnd.domain.order.repository.OrderRepository;
+import sunjin.DeptManagement_BackEnd.global.enums.ApprovalStatus;
+import sunjin.DeptManagement_BackEnd.global.enums.OrderType;
 import sunjin.DeptManagement_BackEnd.global.enums.Role;
 
 @Component
@@ -19,13 +23,15 @@ public class DataLoader implements ApplicationRunner {
     private final MemberRepository memberRepository;
     private final JdbcTemplate jdbcTemplate;
     private final PasswordEncoder passwordEncoder;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public DataLoader(DepartmentRepository departmentRepository, MemberRepository memberRepository, JdbcTemplate jdbcTemplate, PasswordEncoder passwordEncoder) {
+    public DataLoader(DepartmentRepository departmentRepository, MemberRepository memberRepository, JdbcTemplate jdbcTemplate, PasswordEncoder passwordEncoder, OrderRepository orderRepository) {
         this.departmentRepository = departmentRepository;
         this.memberRepository = memberRepository;
         this.jdbcTemplate = jdbcTemplate;
         this.passwordEncoder = passwordEncoder;
+        this.orderRepository = orderRepository;
     }
 
     @Override
@@ -40,13 +46,10 @@ public class DataLoader implements ApplicationRunner {
 
         Department hrDepartment = new Department("HR", "Human Resources");
         Department financeDepartment = new Department("FIN", "Finance");
-        Department itDepartment = new Department("IT", "IT");
-        Department adminDepartment = new Department("Admin", "Admin");
+
 
         departmentRepository.save(hrDepartment);
         departmentRepository.save(financeDepartment);
-        departmentRepository.save(itDepartment);
-        departmentRepository.save(adminDepartment);
 
         // 멤버 초기 데이터 삽입
         String encodedPassword1 = passwordEncoder.encode("1234");
@@ -65,6 +68,20 @@ public class DataLoader implements ApplicationRunner {
         memberRepository.save(member3);
         memberRepository.save(member4);
 
+        Order order1 = new Order(OrderType.FOOD_COSTS, "수지네 식당", 35000, "부서 단체 식사", ApprovalStatus.WAIT, null, "a", "a", null, null, member1, member1.getDepartment());
+        Order order2 = new Order(OrderType.FIXTURES, "A4", 8700, "A4용지 구매", ApprovalStatus.WAIT, null, "a", "a", null, null, member1, member1.getDepartment());
+        Order order3 = new Order(OrderType.TRANSPORTATION, "출장", 48000, "판교 출장으로 KTX 결제", ApprovalStatus.WAIT, null, "a", "a", null, null, member1, member1.getDepartment());
+
+        Order order4 = new Order(OrderType.SNACK, "쿠팡", 32000, "간식 구매", ApprovalStatus.WAIT, null, "a", "a", null, null, member2, member2.getDepartment());
+        Order order5 = new Order(OrderType.ENTERTAINMENT, "풍성옥", 52000, "판교팀 접대", ApprovalStatus.WAIT, null, "a", "a", null, null, member2, member2.getDepartment());
+        Order order6 = new Order(OrderType.ETC, "출장비", 5100, "물품 구매", ApprovalStatus.WAIT, null, "a", "a", null, null, member2, member2.getDepartment());
+
+        orderRepository.save(order1);
+        orderRepository.save(order2);
+        orderRepository.save(order3);
+        orderRepository.save(order4);
+        orderRepository.save(order5);
+        orderRepository.save(order6);
 
         System.out.println("초기 데이터 삽입 완료!");
     }
