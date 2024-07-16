f2f9b10e25dfe7fe2d362419eda4ec6f5b5fdb17
seyun <nkje0139@naver.com>
2024-07-16 12:49:53 +0900

    feat : 주문 조회시 사용되는 메서드 추가

--- src/main/java/sunjin/DeptManagement_BackEnd/domain/order/repository/OrderRepository.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/repository/OrderRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import sunjin.DeptManagement_BackEnd.domain.department.domain.Department;
 import sunjin.DeptManagement_BackEnd.domain.order.domain.Order;
 import sunjin.DeptManagement_BackEnd.global.enums.ApprovalStatus;
 
@@ -18,6 +17,12 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("SELECT o FROM Order o WHERE o.deletedAt IS NULL AND o.member.id = :memberId AND o.status = :status")
     List<Order> findAllByMemberIdAndStatus(@Param("memberId") Long memberId, @Param("status") ApprovalStatus status);
 
-    @Query("SELECT o FROM Order o WHERE o.deletedAt IS NULL AND o.member.id = :memberId AND o.status IN :statuses")
-    List<Order> findByMemberIdAndStatusIn(@Param("memberId") Long memberId, @Param("statuses") List<ApprovalStatus> statuses);
+    @Query("SELECT o FROM Order o WHERE o.deletedAt IS NULL AND o.member.id = :memberId AND o.status IN :status")
+    List<Order> findByMemberIdAndStatusIn(@Param("memberId") Long memberId, @Param("status") List<ApprovalStatus> status);
+
+    @Query("SELECT o FROM Order o WHERE o.deletedAt IS NULL AND o.status = :status")
+    List<Order> findByStatus(@Param("status") ApprovalStatus status);
+
+    @Query("SELECT o FROM Order o WHERE o.deletedAt IS NULL AND o.status = :status")
+    List<Order> findByStatusIsProgress(@Param("status") ApprovalStatus status);
 }
