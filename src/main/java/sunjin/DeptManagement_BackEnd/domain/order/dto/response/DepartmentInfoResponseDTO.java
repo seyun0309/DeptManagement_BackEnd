b5e244929fad319708c8837039160de020446dc1
seyun <nkje0139@naver.com>
2024-07-16 12:51:07 +0900

    feat : 부서 이름, 사원 이름을 포함한 응답 DTO 추가

---
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/dto/response/DepartmentInfoResponseDTO.java
@@ -0,0 +1,15 @@
+package sunjin.DeptManagement_BackEnd.domain.order.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class DepartmentInfoResponseDTO {
+    private String deptName;
+    private List<MemberResponseDTO> members;
+}
