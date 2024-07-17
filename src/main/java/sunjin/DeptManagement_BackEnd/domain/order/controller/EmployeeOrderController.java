ab24c3d296b24301834b06b4c61f2c0c2762ea8d
seyun <nkje0139@naver.com>
2024-07-17 14:25:57 +0900

    feat : 명세서 수정 및 @RequestParam string으로 받음

--- src/main/java/sunjin/DeptManagement_BackEnd/domain/order/controller/EmployeeOrderController.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/domain/order/controller/EmployeeOrderController.java
@@ -20,6 +20,7 @@ import sunjin.DeptManagement_BackEnd.global.error.exception.BusinessException;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -38,28 +39,28 @@ public class EmployeeOrderController {
     }
 
     @GetMapping("/employee/{orderId}")
-    @Operation(summary = "[사원] 주문 상세 조회", description = "신청한 주문의 상세 내역을 조회합니다")
+    @Operation(summary = "[사원] 본인의 주문 상세 조회", description = "신청한 주문의 상세 내역을 조회합니다")
     public ResponseEntity<GetOrderDetailResponseDTO> getOrder(@PathVariable("orderId") Long orderId) throws IOException {
         GetOrderDetailResponseDTO response = commonOrderService.getOrderDetails(orderId);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/employee/submit")
-    @Operation(summary = "[사원] 주문 상신", description = "주문을 선택하여 팀장에게 상신합니다.")
-    public ResponseEntity<String> submitOrder(@RequestParam(value = "id", required = false) List<Long> id){
-        employeeOrderService.submitOrder(id);
+    @Operation(summary = "[사원] 본인의 주문을 팀장에게 상신", description = "주문을 선택하여 팀장에게 상신합니다.")
+    public ResponseEntity<String> submitOrder(@RequestParam(value = "order", required = false) List<String> ids){
+        employeeOrderService.submitOrder(ids.stream().map(Long::parseLong).collect(Collectors.toList()));
         return ResponseEntity.ok("상신에 성공했습니다.");
     }
 
     @GetMapping("/employee/orders")
-    @Operation(summary = "[사원] 전체 및 상태별 조회", description = "전체 및 상태별 조회가 진행됩니다")
+    @Operation(summary = "[사원] 본인의 주문을 전체 및 상태별 조회", description = "전체 및 상태별 조회가 진행됩니다")
     public ResponseEntity<List<?>> getAllOrder(@RequestParam(value = "status", required = false) String status) {
         List<?> response = commonOrderService.getOrders(status);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/employee/img/{orderId}")
-    @Operation(summary = "[사원] 수정 버튼 클릭", description = "수정 버튼을 클릭하면 해당 주문의 사진을 리턴합니다")
+    @Operation(summary = "[사원] 수정 모달에 띄울 이미지 리턴", description = "수정 버튼을 클릭하면 해당 주문의 사진을 리턴합니다")
     public ResponseEntity<Resource> getImg(@PathVariable("orderId") Long orderId) {
         try {
             Resource resource = commonOrderService.getImg(orderId);
@@ -76,7 +77,7 @@ public class EmployeeOrderController {
     }
 
     @PatchMapping("/employee/{orderId}")
-    @Operation(summary = "[사원] 주문 수정", description = "물품 타입, 물품 이름, 개당 가격, 수량를 입력하면 물품 수정이 진행됩니다")
+    @Operation(summary = "[사원] 본인의 주문 수정", description = "물품 타입, 물품 이름, 개당 가격, 수량를 입력하면 물품 수정이 진행됩니다")
     public ResponseEntity<String> updateOrder(@RequestPart(name = "image") MultipartFile image,
                                               @RequestPart(name = "request") @Valid createOrderRequestDTO createOrderRequestDTO,
                                               @PathVariable("orderId") Long orderId){
@@ -85,7 +86,7 @@ public class EmployeeOrderController {
     }
 
     @DeleteMapping("/employee/{orderId}")
-    @Operation(summary = "[사원] 주문 삭제", description = "URL의 orderID를 통해 해당 물품 삭제가 진행됩니다.")
+    @Operation(summary = "[사원] 본인의 주문 삭제", description = "URL의 orderID를 통해 해당 물품 삭제가 진행됩니다.")
     public ResponseEntity<String> deleteOrder(@PathVariable("orderId") Long orderId){
         commonOrderService.deleteOrder(orderId);
         return ResponseEntity.ok("주문 삭제에 성공했습니다.");
