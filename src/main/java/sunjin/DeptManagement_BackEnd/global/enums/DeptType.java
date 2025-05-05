d990b349126ffaab3ee9f7949b1835fce760f217
seyun0309 <nkje0309@gmail.com>
2025-05-05 14:45:31 +0900

    FEAT : 부서 코드를 통해 부서를 찾는 로직 추가

--- src/main/java/sunjin/DeptManagement_BackEnd/global/enums/DeptType.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/global/enums/DeptType.java
@@ -1,13 +1,14 @@
 package sunjin.DeptManagement_BackEnd.global.enums;
 
 import lombok.Getter;
+import sunjin.DeptManagement_BackEnd.global.error.exception.BusinessException;
 
 @Getter
 public enum DeptType {
-    DIGITAL_SCM("digital_scm","디지털 SCM"),
-    DIGITAL_FCM("digital_fcm","디지털 FCM"),
-    DATA_INNOVATION("data_innovation","데이터 이노베이션"),
-    ACCOUNTANCY("accountancy", "회계");
+    DIGITAL_SCM("0123","디지털 SCM"),
+    DIGITAL_FCM("4812","디지털 FCM"),
+    DATA_INNOVATION("5812","데이터 이노베이션"),
+    ACCOUNTANCY("8231", "회계");
 
     private final String code ;
     private final String description;
@@ -16,4 +17,14 @@ public enum DeptType {
         this.code = code;
         this.description = description;
     }
+
+    public static DeptType formCode(String code) {
+        for (DeptType type : DeptType.values()) {
+            if (type.code.equalsIgnoreCase(code)) {
+                return type;
+            }
+        }
+        throw new BusinessException(ErrorCode.DEPARTMENT_NOT_FOUND);
+    }
+
 }
