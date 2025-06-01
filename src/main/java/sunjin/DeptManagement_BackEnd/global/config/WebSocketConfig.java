3c300f21c467c433d201dc58b1caa77563fe3be7
seyun kim <>
2025-06-01 20:20:15 +0900

    FEAT : 웹소캣 최초 연결시 토큰이 필요한 로직으로 변경

--- src/main/java/sunjin/DeptManagement_BackEnd/global/config/WebSocketConfig.java
+++ src/main/java/sunjin/DeptManagement_BackEnd/global/config/WebSocketConfig.java
@@ -10,17 +10,25 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final JwtHandshakeInterceptor handshakeInterceptor;
+
+    public WebSocketConfig(JwtHandshakeInterceptor handshakeInterceptor) {
+        this.handshakeInterceptor = handshakeInterceptor;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*") // CORS 허용
-                .withSockJS(); // SockJS fallback
+                .addInterceptors(handshakeInterceptor)
+                .setHandshakeHandler(new CustomHandshakeHandler())
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app"); // 클라이언트 → 서버 전송
-        registry.enableSimpleBroker("/user", "/topic"); // 서버 → 클라이언트 브로드캐스트
-        registry.setUserDestinationPrefix("/user"); // 특정 사용자에게 보낼 때 prefix
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/user", "/topic");
+        registry.setUserDestinationPrefix("/user");
     }
 }
