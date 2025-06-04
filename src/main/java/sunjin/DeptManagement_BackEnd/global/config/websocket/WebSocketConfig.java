3d4d4c73d2e3090fea74a80ab82a3ea3979ba2a7
seyun kim <>
2025-06-04 19:07:05 +0900

    FEAT : 파일 위치 이동 및 stomp -> websocket 방식으로 전환

---
+++ src/main/java/sunjin/DeptManagement_BackEnd/global/config/websocket/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package sunjin.DeptManagement_BackEnd.global.config.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final SocketTextHandler handler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/ws/notifications")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
